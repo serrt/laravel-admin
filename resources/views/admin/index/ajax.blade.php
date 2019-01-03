f3079c7c5aeafbb3d325463032c2a1d64c189ba4
serrt <1163816051@qq.com>
2019-01-03 11:37:19 +0800

    file

--- resources/views/admin/index/ajax.blade.php
+++ resources/views/admin/index/ajax.blade.php
@@ -1,24 +1,17 @@
 @extends('admin.layouts.iframe')
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-info">
-                <div class="box-header with-border">
-                    <div class="box-title">多文件管理</div>
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <div class="box-title">多文件管理</div>
+        </div>
+        <div class="box-body">
+            <form action="" class="form-horizontal" role="form">
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <input type="file" name="files2" class="form-control file-input3" data-upload-url="{{route('api.web.upload')}}" data-initial-preview="{{$imgs_url}}" multiple>
+                    </div>
                 </div>
-                <div class="box-body">
-                    <form action="" class="form-horizontal" role="form">
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                <input type="file" name="files2" class="form-control file-input3"
-                                       data-upload-url="{{route('api.web.upload')}}"
-                                       data-initial-preview="{{$imgs_url}}"
-                                       multiple>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 @endsection
