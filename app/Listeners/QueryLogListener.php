2eeec4a0c301cca3fa6b6afcdd680b7bcad8958c
serrt <1163816051@qq.com>
2019-06-06 08:56:27 +0800

    update QueryLogListener

--- app/Listeners/QueryLogListener.php
+++ app/Listeners/QueryLogListener.php
@@ -26,12 +26,30 @@ class QueryLogListener
      */
     public function handle($event)
     {
-        $sql = str_replace("?", "'%s'", $event->sql);
-
-        $log = vsprintf($sql, $event->bindings);
+        $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $event->sql);
+        $bindings = $event->connection->prepareBindings($event->bindings);
+        $pdo = $event->connection->getPdo();
+        $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+        $duration = $this->formatDuration($event->time / 1000);
 
         if (config('app.env') != 'production') {
-            Log::channel('query')->info($log." [".$event->time." ms]");
+            Log::channel('query')->debug(sprintf('[%s] %s | %s: %s', $duration, $realSql, request()->method(), request()->getRequestUri()));
         }
+    }
+    /**
+     * Format duration.
+     *
+     * @param float $seconds
+     *
+     * @return string
+     */
+    private function formatDuration($seconds)
+    {
+        if ($seconds < 0.001) {
+            return round($seconds * 1000000).'μs';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000, 2).'ms';
+        }
+        return round($seconds, 2).'s';
     }
 }
